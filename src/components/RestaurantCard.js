151682b6775ab6f7169726f4f4559fc64c86e4d7
shwetakawar <shwetakawar5@gmail.com>
2025-05-05 21:53:51 +0530

    adeed rating icon

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -18,17 +18,18 @@ const RestaurantCard = (props) => {
         src={IMG_URL + cloudinaryImageId}
       />
       <h3 className="text-xl font-semibold">{name}</h3>
-      <div className="flex justify-start items-center font-normal text-base text-gray-700">
+      <div className="flex justify-start items-center font-normal text-base text-gray-700 gap-x-2">
         <svg
+          width="20"
+          height="20"
+          viewBox="0 0 512 512"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="size-4 text-green-800 rounded-full p-2 flex"
         >
-          <path
-            fillRule="evenodd"
-            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-            clipRule="evenodd"
+          <title>Star Icon</title>
+          <circle cx="256" cy="256" r="233" fill="#2BB673" />
+          <polygon
+            fill="#FFFFFF"
+            points="256,119.4 300.4,209.3 399.6,223.8 327.8,293.8 344.8,392.6 256,345.9 167.2,392.6 184.2,293.8 112.4,223.8 211.6,209.3"
           />
         </svg>
 
